b85b52b3cb83f2dbfdeda55f6b737cdfb1b34ca9
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-03-29 12:01:47 -0300

    ATUALIZAÇÃO

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" href="Imagens\vecteezy_head-viking-mascot-esports-logo-vector-illustration_.jpg">
+        <title>Login - Taverna</title>
+    </head>
+    <body>
+        <div class="container">
+        <div class="box-parent-login">
+        <div class="well bg-white box-login">
+            <h1 class="ls-login-logo">Taverna</h1>
+            <form role="form">
+                <fieldset>
+    
+                    <div class="form-group ls-login-user">
+                        <label for="userLogin">Usuário</label>
+                        <input class="form-control ls-login-bg-user input-lg" id="userLogin" type="text" aria-label="Usuário" placeholder="Usuário">
+                    </div>
+    
+                    <div class="form-group ls-login-password">
+                        <label for="userPassword">Senha</label>
+                        <input class="form-control ls-login-bg-password input-lg" id="userPassword" type="password" aria-label="Senha" placeholder="Senha">
+                    </div>
+    
+                    <a href="#" class="ls-login-forgot">Esqueci minha senha</a>
+    
+                    <p class="txt-center ls-login-signup">Não possui um usuário na Taverna?
+                        <a href="#">Cadastre-se agora</a>
+                    </p>
+                    <input type="submit" value="Entrar" class="btn btn-primary btn-lg btn-block">
+                </fieldset>
+            </form>
+        </div>
+    </div>
+        </div>
+    </body>
+</html>
