8bbd9d53d8bd659503c8681fe889ad5701a4caf9
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-03-29 11:43:03 -0300

    JUAN

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dnsdadat</title>
+</head>
+<body>
+    sodium_crypto_sign_detached
+</body>
+</html>
