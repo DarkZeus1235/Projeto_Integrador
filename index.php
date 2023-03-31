207f3e9e31d9fefc1d40d810c25de606f916faee
maluburra <marialuizasesc12@gmail.com>
2023-03-31 08:43:56 -0300

    Atualização

--- index.php
+++ index.php
@@ -1,12 +1,60 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Taverna de Valhalla</title>
+  </head>
+  <body>
+    <div class="menu-bar">
+      <h1 class="logo">𝕿𝖆𝖛𝖊𝖗𝖓𝖆 𝖉𝖊 <span>𝖁𝖆𝖑𝖍𝖆𝖑𝖑𝖆</span></h1>
+      <ul>
+        <li><a href="#">Vinho</a></li>
+        <li><a href="#">Conhaque</a></li>
+        <li><a href="#">Rum</a></li>
+        <li><a href="#">Cerveja</a></li>
+        <li><a href="#">Whisky<i class="fas fa-caret-down"></i></a>
+
+            <div class="dropdown-menu">
+                <ul>
+                  <li><a href="#">Whisky Escocês</a></li>
+                  <li><a href="#">Whisky Single Malt</a></li>
+                  <li><a href="#">Whisky de grão</a></li>
+                  <li><a href="#">Whisky Single Malt</a></li>
+                  <li><a href="#">Blended Scotch Whisky</a></li>
+                  <li><a href="#">Whisky Irlandês</a></li>
+                  <li><a href="#">Whisky Japonês</a></li>
+                  <li><a href="#">Bourbon</a></li>
+                  <li><a href="#">Straight bourbon</a></li>
+                  <li><a href="#">Blended bourbon</a></li>
+                  <li><a href="#">Tenesse Whiskey</a></li>
+                  <li><a href="#">Rye</a></li>
+                  <li><a href="#">Whiskies saborizados</a></li>
+                  <li><a href="#">Licores de Whisky</a></li>
+                </ul>
+              </div>
+        </li>
+        <li><a href="#">Vodka</a>
+        </li>
+        <li><a href="#">Tequila</a></li>
+        <li><a href="#">Delivery</a></li>
+        <li><a href="#">Fale conosco</a></li>
+        <form class="d-flex" role="search">
+        <input class="form-control me-2" type="pesquisar" placeholder="Pesquisar" aria-label="Pesquisar">
+        <button class="btn" type="submit">Pesquisar</button>
+      </form>
+      </ul>
+    </div>
+
+    <div class="hero">
+      &nbsp;
+    </div>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 </html>
