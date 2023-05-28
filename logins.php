4629b1e29ebd46e6d18504c8af424de2b9a621b8
unknown <jpfernandes@outlook.com>
2023-05-28 10:09:17 -0300

    ATUALIZAÇÃO

---
+++ logins.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/logins.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Login - Taverna de Valhalla</title>
+</head>
+<body>
+    
+<header class="header"> 
+    <nav class="navbar">
+        <a href="">Início</a>
+        <a href="">Sobre</a>
+        <a href="">Serviços</a>
+        <a href="">Contato</a>
+    </nav>
+
+    <form action="#" class="search-bar">
+        <input type="text" placeholder="Pesquisar">
+        <button class="submit"><i class="bx bx-search"></i></button>
+    </form>
+</header>
+<div class="container"></div>
+</body>
+</html>
