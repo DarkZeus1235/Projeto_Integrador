8ce3b5c2938754c28ececc1c47ecd7d9a3fe46cc
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-05-29 08:22:21 -0300

    atualização

--- logins.php
+++ logins.php
@@ -23,6 +23,28 @@
         <button class="submit"><i class="bx bx-search"></i></button>
     </form>
 </header>
-<div class="container"></div>
+<div class="container">
+<div class="login-container">
+    <h2>Faça login</h2>
+    <form action="login.php" method="post">
+      <div class='inputbox'>
+      <ion-icon name="mail-outline"></ion-icon>
+      <input type="text" name="email" placeholder="Email" required>
+      </div>
+      <div class="inputbox">
+        <ion-icon name="lock-closed-outline"></ion-icon>
+        <input type="password" name="senha" placeholder="Senha" required>
+      </div>
+      <input type="submit" value="Entrar">
+    </form>
+    <div class='register'>
+    <p>Não tem uma conta? <a href="cadastro.php">Registre-se</a></p>
+    </div>
+  </div>
+</body>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</html>
+</div>
 </body>
 </html>
