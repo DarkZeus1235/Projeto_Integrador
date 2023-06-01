95d89c56b48bc4e19ebfa8a1dccb615acf7b8753
JUAN VITOR TASCA DE ANDRADE <juan.4910@aluno.pr.senac.br>
2023-06-01 14:35:34 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-console.log("Testando");
+// função que adiciona tarefa
+function addTask(){
+    // titulo da tarefa
+    const taskTitle = document.querySelector("#task-title").value;
+    if(taskTitle){
+        // clona template  
+        const template = document.querySelector(".template");
+        const newTask = template.cloneNode(true);
+        console.log(newTask);
+        // adiciona um título
+        newTask.querySelector(".task-title").textContent = taskTitle;
+        // remover classes desnecessárias
+        newTask.classList.remove("template");
+        newTask.classList.remove("hide");
+        // adiciona tarefa na lista
+        const list = document.querySelector("#task-list");
+        list.appendChild(newTask);
+        // adicionar evento de remover 
+        const removeBtn = newTask.querySelector(".remove-btn").addEventListener("click", function(){
+            removeTask(this);
+        });
+        // limpar texto
+        document.querySelector("#task-title").value = "";
+        // adicionar evento de concluir tarefa
+    }
+}
+// função de remover tarefa
+function removeTask(task){
+    task.parentNode.remove(true);
+}
+// evento de adicionar tarefa
+const addBtn = document.querySelector("#add-btn");
+addBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    addTask();
+})
