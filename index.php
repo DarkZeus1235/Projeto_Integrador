0b8498a87d9c0038037bf5252248a3562dca551d
MATHEUS HENRIQUE VITAL <matheusvital029@gmail.com>
2023-06-07 07:35:00 -0300

    nbef

--- index.php
+++ index.php
@@ -58,54 +58,6 @@
   <div class="hero">
     &nbsp;
   </div>
-<div class="card" style="width: 18rem;">
-        <img id="image"  src="vin/vinho1.jpg" class="card-img-top" alt="" width="90">
-        <div class="card-body">
-          <h5 class="card-title">VINHO CHIANTI</h5>
-          <p class="card-text">200,00$</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    <div class="card" style="width: 18rem;">
-        <img id="image"  src="vin/vinhoamarone.webp" class="card-img-top" alt="" width="90">
-        <div class="card-body">
-          <h5 class="card-title">Vinho Amarone Montresor Capitel della Crosara</h5>
-          <p class="card-text">540,00$</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    <div class="card" style="width: 18rem;">
-        <img id="image"  src="vin/vinhoamaronedella.webp" class="card-img-top" alt="" width="90">
-        <div class="card-body">
-          <h5 class="card-title">Vinho Amarone della Valpolicella Terre di Verona</h5>
-          <p class="card-text">308,00$</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    <div class="card" style="width: 18rem;">
-        <img id="image"  src="vin/vinhocompenhage.jpg" class="card-img-top" alt="" width="90">
-        <div class="card-body">
-          <h5 class="card-title">Champagne Moët Impérial Brut 750ml</h5>
-          <p class="card-text">430,00$</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    <div class="card" style="width: 18rem;">
-        <img id="image"  src="vin/vinhocompenhagedom.jpgs" class="card-img-top" alt="" width="90">
-        <div class="card-body">
-          <h5 class="card-title">Champagne Dom Pérignon Brut 750ml</h5>
-          <p class="card-text">2.000,00$</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    <div class="card" style="width: 18rem;">
-        <img id="image"  src="vin/vinhocompenhagedom.jpg" class="card-img-top" alt="" width="90">
-        <div class="card-body">
-          <h5 class="card-title">..</h5>
-          <p class="card-text">00,00$</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </html>
