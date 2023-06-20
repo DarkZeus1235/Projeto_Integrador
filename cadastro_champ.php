4336af46b3ba38b009612548d8f038e2754c46a2
MATHEUS HENRIQUE VITAL <matheusvital029@gmail.com>
2023-06-20 10:15:38 -0300

    hefgru

--- cadastro_champ.php
+++ cadastro_champ.php
@@ -54,7 +54,7 @@ if (isset($_POST['bt_nome'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <title>Cadastro de Bebidas</title>
+    <title>Cadastro de Champagne</title>
 </head>
 
 <body>
@@ -114,7 +114,7 @@ if (isset($_POST['bt_nome'])) {
         </ul>
     </div>
     <div class="container">
-        <h1>Cadastro de Bebidas - Taverna de Valhalla</h1>
+        <h1>Cadastro de Champagne - Taverna de Valhalla</h1>
         <form action="" method="post" enctype="multipart/form-data">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Nome da Bebida:</label>
--- lista_vinhos.php
+++ lista_vinhos.php
@@ -16,7 +16,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <link rel="stylesheet" href="css/style.css">
         <link rel="icon" href="img/hamburguer-queijo-com-ilustracao-do-icone-do-vetor-dos-desenhos-animados-do-fogo-conceito-de-icone-de-objeto-de-comida-isolado-premium_138676-5539.avif">
-        <title>Lista de Bebidas</title>
+        <title>Lista de Vinhos</title>
     </head>
 
     <body>
