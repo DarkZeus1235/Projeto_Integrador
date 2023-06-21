33b13d56a31243b165e81852a46d0f9e3b3e6cd0
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2023-06-21 12:05:14 -0300

    ATT

--- comprar.php
+++ comprar.php
@@ -13,7 +13,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="icon" href="Imagens/vikingpinguço.png">
+    <link rel="icon" href="img/vikingpinguço.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Taverna de Valhalla</title>
     </head>
