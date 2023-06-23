d27e328771a52cb4f55c10e7a6fb1df4de6e0d96
MARIA LUIZA LIMA DE OLIVEIRA <oliveira.0936@aluno.pr.senac.br>
2023-06-23 08:58:47 -0300

    ATT

--- index.php
+++ index.php
@@ -16,8 +16,7 @@ $quantidade_pedidos = $retorno_consulta->num_rows;
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
   <link rel="icon" href="Imagens/vikingpinguço.png">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -90,7 +89,29 @@ $quantidade_pedidos = $retorno_consulta->num_rows;
 
     </ul>
   </div>
+ 
   <div class="container">
+  <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="Imagens/bebida.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="Imagens/bebida.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Imagens/bebida.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     <?php
     if (isset($_SESSION['nome'])) {
       ?>
@@ -102,7 +123,11 @@ $quantidade_pedidos = $retorno_consulta->num_rows;
 
     }
     ?>
+
+    
+    
     <h1 id="destaque">Produtos em Destaque</h1>
+    
     <!-- Onde vai aparecer as pedidas após cadastradas no banco de dados -->
     <div class="row">
       <?php
@@ -141,7 +166,6 @@ $quantidade_pedidos = $retorno_consulta->num_rows;
   </div>
 
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 
 </html>
