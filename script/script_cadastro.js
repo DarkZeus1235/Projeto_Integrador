3c2a4a1be1c054b45fb8bb5b9de73b667063fb49
JUAN VITOR TASCA DE ANDRADE <juan.4910@aluno.pr.senac.br>
2023-06-23 12:07:48 -0300

    sfhfdsbhyhgh

--- script/script_cadastro.js
+++ script/script_cadastro.js
@@ -1,12 +1,14 @@
 const ConfirmRegister = document.getElementsByClassName("input-register-confirm");
-for (var i = 0; i < ConfirmRegister.length; i++)
+for (var i = 0; i < ConfirmRegister.length; i++){
+    ConfirmRegister[i].addEventListener("click", validateFields)
+}
 
 
-function validateFiels(){
+function validateFields(){
     var inputs = document.getElementsByClassName("input-register-password");
     var controle = false;
     for (var i = 0; i < inputs.length; i++){
-        if(inputs[i].ariaValueMax.trim() !== ''){
+        if(inputs[i].value.trim() !== ''){
             controle = true;
             break;
         }
@@ -21,13 +23,8 @@ function validateFiels(){
     }
 }
 function showAlert(){
-    Swal.fire({
-        text: 'Você conseguiu se cadastrar!',
-        icon: 'success',
-        title: 'Sucesso',
-    }) .then((result) => {
-        if(result.isConfirmed){
-            window.location.href = "login.php";
-        }
-    })
+    if(result.isConfirmed){
+        alert("Cadastro realizado com sucesso!")
+    }
+   
 }
