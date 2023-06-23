5acfa0a169a1461a22134a1167e128699d251d02
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-06-23 07:37:24 -0300

    att

--- vodka.php
+++ vodka.php
@@ -40,25 +40,22 @@ $quantidade_pedidos = $retorno_consulta->num_rows;
 
 
 <div class="dropdown-menu">
-          <ul>
-            <li><a href="vinhos.php">Vinhos</a></li>
-            <li><a href="whisky.php">Uísque</a></li>
-            <li><a href="vodka.php">Vodka</a></li>
-            <li>
-              <a href="#">Outros <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                  class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                  <path
-                    d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-                </svg></a>
+                    <ul>
+                        <li><a href="vinhos.php">Vinhos</a></li>
+                        <li><a href="whisky.php">Uísque</a></li>
+                        <li>
+                            <a href="#">Outros <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+                                </svg></a>
 
-              <div class="dropdown-menu-1">
-                <ul>  
-                  <li><a href="conhaque.php">Conhaque</a></li>
-                  <li><a href="rum.php">Rum</a></li>
-                  <li><a href="champ.php">Champanhe</a></li>
-                  <li><a href="gin.php">Gin</a></li>
-                </ul>
-              </div>    
+                            <div class="dropdown-menu-1">
+                                <ul>
+                                    <li><a href="conhaque.php">Conhaque</a></li>
+                                    <li><a href="rum.php">Rum</a></li>
+                                    <li><a href="champ.php">Champanhe</a></li>
+                                    <li><a href="vodka.php">Vodka</a></li>
+                                </ul>
+                            </div>
 
                         </li>
                     </ul>
