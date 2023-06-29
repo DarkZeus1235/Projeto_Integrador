0d0cd3b7e1556dbebff46c8706717528350978af
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-06-29 11:48:09 -0300

    att

--- cadastro.php
+++ cadastro.php
@@ -57,13 +57,47 @@ if (isset($_POST['bt_nome'])) {
             <input class="input-register-username" type="text" name="bt_username" placeholder="Crie um nome de Usuário" required>
             <input class="input-register-cpf" type="text" name="bt_cpf" placeholder="CPF" required>
             <input class="input-type-address" type="text" name="bt_endereco" placeholder="Endereço" required>
-            <input class="input-register-confirm" type="submit" value="Cadastrar">
+            <input type="submit" value="Cadastrar" onclick="return validateFields()">
             <p class="plv">Já tem uma conta?</p>
             <p><a id="entrar" href="login.php">Entrar</a></p>
         </form>
+        <script>
+    function validateFields() {
+      var inputs = document.querySelectorAll('input[type="text"], input[type="password"]');
+      var controle = false;
+
+      for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value.trim() !== '') {
+          controle = true;
+          break;
+        }
+      }
+
+      if (controle) {
+        showAlert();
+        return false; // Impede o envio do formulário
+      } else {
+        controle = false;
+        inputs = false;
+        return true; // Permite o envio do formulário
+      }
+    }
+
+    function showAlert() {
+      Swal.fire({
+        text: 'Sucesso ao cadastrar.',
+        icon: 'success',
+        title: 'Sucesso',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = "login.php";
+        }
+      });
+    }
+  </script>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-<script src="script/script_cadastro.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.12/dist/sweetalert2.all.min.js"></script>  
 
 </html>
