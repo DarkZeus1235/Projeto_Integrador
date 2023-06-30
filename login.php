61e2b90d50c4a20e1729e08ab86c8a70ae2ad21c
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2023-06-30 08:57:06 -0300

    att

--- login.php
+++ login.php
@@ -44,7 +44,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
 <html lang="pt-br">
 <html>
 <head>
-<link rel="stylesheet" href="css/login.css">
+<link rel="stylesheet" href="css/style.css">
 <link rel="icon" href="img/hamburguer-queijo-com-ilustracao-do-icone-do-vetor-dos-desenhos-animados-do-fogo-conceito-de-icone-de-objeto-de-comida-isolado-premium_138676-5539.avif">
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 <title>Página de Login</title>
