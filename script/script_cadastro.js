027f2db21da0165b2b394eb988de0c5a02d40ff8
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-08-02 08:49:43 -0300

    att

--- script/script_cadastro.js
+++ script/script_cadastro.js
@@ -1,36 +1,6 @@
-const ConfirmRegister = document.getElementsByClassName("input-register-confirm");
-for (var i = 0; i < ConfirmRegister.length; i++){
-    ConfirmRegister[i].addEventListener("click", validateFields)
-}
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
 
-
-function validateFields(){
-    var inputs = document.getElementsByClassName("input-register-password");
-    var controle = false;
-    for (var i = 0; i < inputs.length; i++){
-        if(inputs[i].value.trim() !== ''){
-            controle = true;
-            break;
-        }
-    }
-    if(controle){
-        showAlert();
-        return false; // Impede envio do formulário
-    } else{
-        controle = false;
-        inputs = false;
-        return true; // Permite envio do formulário 
-    }
-}
-function showAlert(){
-    showAlert.fire({
-        text: 'Sucesso ao cadastrar',
-        icon: 'success',
-        title: 'Sucesso',
-    }).then((result) => {
-        if(result.isConfirmed){
-            window.location.href = "login.php"
-        }
-    })   
-   
-}
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
