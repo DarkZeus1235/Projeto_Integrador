4bf3cc6083a6ac5c4bde83f60387021fc9dd8632
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-08-09 09:08:43 -0300

    att

--- cadastro.php
+++ cadastro.php
@@ -47,4 +47,5 @@ if (isset($_POST['bt_nome'])) {
     </div>
 </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </html>
