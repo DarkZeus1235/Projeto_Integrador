a62bf32d1f1e3860d2ecd8e8bc7c5a630afc9d76
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2023-08-10 09:07:30 -0300

    att

--- menu.php
+++ menu.php
@@ -1,76 +1,64 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <link rel="stylesheet" href="css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-</head>
-<body>
-    
-</body>
-</html>
-<div class="container">
-    <div class="row">
-        <div class="col">
-        <div class="menu-bar">
-    <img src="Imagens/vikingpinguço.png" alt="">
-    <div class="container">
-        <h1 class="logo">𝓣𝓪𝓿𝓮𝓻𝓷𝓪 𝓭𝓮 <span>𝓥𝓪𝓵𝓱𝓪𝓵𝓵𝓪</span></h1>
-    </div>
-
-    <ul>
-        <li><a href="index.php">Início</a></li>
-        <li><a href="#">Promoções</a></li> <!-- Sem link -->
-        <li><a href="#">Novidades</a> <!-- Sem link -->
-        <li><a href="#">+ Vendidos <i class="bi bi-caret-down"></i></a> <!-- Sem link -->
-            <div class="dropdown-menu">
-                <ul>
-                    <li><a href="vinhos.php">Vinhos</a></li>
-                    <li><a href="whisky.php">Uísque</a></li>
-                    <li>
-                        <a href="#">Outros <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-                            </svg></a>
-
-                        <div class="dropdown-menu-1">
-                            <ul>
-                                <li><a href="conhaque.php">Conhaque</a></li>
-                                <li><a href="rum.php">Rum</a></li>
-                                <li><a href="champ.php">Champanhe</a></li>
-                                <li><a href="vodka.php">Vodka</a></li>
-                            </ul>
-                        </div>
-
+<header>
+    <nav class="navbar navbar-expand-lg custom-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="Imagens/vikingpinguço.png" alt="" class="logo-img">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="contato.php">Contato</a>
                     </li>
-                </ul>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre nós</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Promoções</a>
+</li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                            </svg></i>
+                            Minha conta</a>
+                        <ul class="dropdown-menu">
+                            <?php
+                            if (!isset($_SESSION['nome'])) {
+                            ?>
+                                <li class="nav-item">
+                                    <a id="nav-link2" class="nav-link" href="login.php">Entrar</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a id="nav-link" class="nav-link" href="cadastro.php">Cadastrar</a>
+                                </li>
+                            <?php
+                            } else {
+                            ?>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="logout" href="logout.php">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+                                            <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                                        </svg>
+                                        Sair</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="conta.php">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lock" viewBox="0 0 16 16">
+                                            <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 5.996V14H3s-1 0-1-1 1-4 6-4c.564 0 1.077.038 1.544.107a4.524 4.524 0 0 0-.803.918A10.46 10.46 0 0 0 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h5ZM9 13a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-2Zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1Z" />
+                                        </svg>
+                                        Minhas Informações</a>
+                                </li>
+                            <?php
+                            }
+                            ?>
+                        </ul>
+                    </form>
             </div>
-        </li>
-        <li><a href="contato.php">Fale Conosco</a></li>
-        <?php
-            if (isset($_SESSION['nome'])) {
-        ?>
-        <li><a href="#" class="log__Conta"> <i class="fa fa-user"></i><?php echo $_SESSION['nome']; ?> </a>
-            <div class="dropdown-menu">
-                <ul>
-                    <li><a href="conta.php" class=""><i class="fa fa-user"></i>Minha Conta</a></li>
-                    <li><a href="logout.php">Sair</a></li>
-                    <?php
-
-                        }
-                    ?>
-                    <?php
-                         if (!isset($_SESSION['nome'])) {
-
-                    ?>
-                    <li><a id="entrar-cadastrar" href="login.php">Entrar |</a> <a href="cadastro.php">Cadastrar</a></li>
-                    <?php
-                        }
-                    ?>
-                </ul>
-            </div>
-        </li>
-    </ul>
         </div>
-    </div>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-</div>
+    </nav>
+</header>
