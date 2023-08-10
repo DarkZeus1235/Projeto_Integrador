d614690c1e67dae99f70e0eb5ce4157bc1e6a819
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2023-08-10 12:07:04 -0300

    atualizad

--- index.php
+++ index.php
@@ -54,28 +54,8 @@ $quantidade_pedidos = $retorno_consulta->num_rows;
   <?php
   include('menu.php');
   ?>
+  <img id="img-vikbebendo" src="img/imagem vikings tomando.jpg" alt="">
   <div class="container">
-    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="10000">
-          <img src="Imagens/banner/wine.jpg" width="600px" height="800px" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-          <img src="Imagens/banner/win2.jpg" width="600px" height="800px"class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="Imagens/banner/win33.jpg" width="600px" height="800px" class="d-block w-100" alt="...">
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
     <?php
     if (isset($_SESSION['nome'])) {
     ?>
@@ -173,7 +153,7 @@ $quantidade_pedidos = $retorno_consulta->num_rows;
                             </h5>
                         </div>
                         
-                            <p class="card-text">
+                        <p class="card-text">
                                 <?php echo $bebidas['descricao'] ?>
                             </p>
                                                
