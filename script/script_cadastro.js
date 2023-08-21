f98a16c4eb0a4d5685b908143ef93d6abe484590
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-08-21 08:59:59 -0300

    att

--- script/script_cadastro.js
+++ script/script_cadastro.js
@@ -1,6 +1,30 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
+function validateFields() {
+    var inputs = document.querySelectorAll('input[type="text"], input[type="password"]');
+    var controle = false;
 
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value.trim() !== '') {
+            controle = true;
+            break;
+        }
+    }
+
+    if (controle) {
+        showAlert();
+        return false; // Impede o envio do formulário
+    } else {
+        return true; // Permite o envio do formulário
+    }
+}
+
+function showAlert() {
+    Swal.fire({
+        text: 'Sucesso ao cadastrar.',
+        icon: 'success',
+        title: 'Sucesso',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = "cadastro.php";
+        }
+    });
+}
