ec9ce06565bcb270a2d2e743f0b9ca424979f290
JOAQUIM PEDRO KLIMA SAGIONETI <joaquim.2944@aluno.pr.senac.br>
2023-08-31 14:00:58 -0300

    att

---
+++ novidades.php
@@ -0,0 +1,53 @@
+<?php
+    include('conexao.php');        
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Novidades</title>
+    </head>
+    <body>
+        <?php
+            include('menu.php');
+        ?>
+        <div class="container">
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+        <div id="joaquim1" class="card">
+    <div id="joaquim2" class="card-body">
+        <h1>18/07/2023</h1>
+        <p>Foram adicionados novos lotes de Jagermeister, Buffalo Trace e Johnny Walker. Venha conferir essa maravilha!</p>
+    </div>
+    <div id="joaquim1" class="card">
+    <div id="joaquim2" class="card-body">
+        <h1>22/07/2023</h1>
+        <p>Mudamos o design do site, inserindo uma nova logo da empresa, alterando o menu e adicionando novas bebidas em nosso cardápio: Dalmatore, Rémy Martin e Ballantines. </p>
+    </div>
+    </div>
+    <div id="joaquim1" class="card">
+    <div id="joaquim2" class="card-body">
+        <h1>13/08/2023</h1>
+        <p>Promoção de Dia dos Pais!!! Insira o nosso cupom "PAIPINGUÇO" para receber 10% de desconto em todas as nossas bebidas! Compre agora!</p>
+    </div>
+    </div>
+    <div id="joaquim1" class="card">
+    <div id="joaquim2" class="card-body">
+        <h1>05/09/2023</h1>
+        <p>A Taverna de Valhalla estará inativa em seus serviços no dia 07/09/2023 por conta dos feriados nacionais. Abriremos novamente na próxima semana!!!</p>
+    </div>
+    </div>
+            
+
+        </div>
+    </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</html>
