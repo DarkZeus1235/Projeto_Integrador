4d0cc95bed0e66837f06a2ecb3f515d37daf9474
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2023-09-04 07:50:39 -0300

    aTTUALIZADAP

--- rodape.php
+++ rodape.php
@@ -1,4 +1,4 @@
 <div class="footer text-center">
     <p>&copy; 2023 Taverna de Valhalla. Todos os direitos reservados.</p>
-    <p>Desenvolvido por Mim</p>
+    <p>Desenvolvido por João Pedro Fernandes, João Rafael, Joaquim Sagioneti, Juan Vitor, Maria Luiza e Matheus Vital</p>
 </div>
