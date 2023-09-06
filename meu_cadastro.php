e8423c78e30fdb65520d88212947af0c4234a03a
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-09-06 11:58:01 -0300

    att

--- meu_cadastro.php
+++ meu_cadastro.php
@@ -83,6 +83,14 @@ $quantidade_cadastros = $retorno_consulta->num_rows;
             </div>
         </a>
     </div>
+    <?php
+    include('espacamento.php');
+?>
+<br>
+    <?php
+    include('rodape.php');
+?>
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
