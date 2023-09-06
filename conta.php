9992bd5f592b0a4275992125ea7843b2c16e1ec2
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2023-09-06 11:51:53 -0300

    attuuuaalizacao

--- conta.php
+++ conta.php
@@ -22,6 +22,8 @@ $logins = $retorno_consulta->fetch_assoc();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/dieimes.css">
+    <link rel="stylesheet" href="css/menu_dieimes.css">
     <title>Minha Conta</title>
 </head>
 
@@ -59,6 +61,12 @@ $logins = $retorno_consulta->fetch_assoc();
             <p class="texto_deletar">Deletar conta</p>
         </li>
     </div>
+    <?php
+        include('espacamento.php');
+    ?>
+    <?php
+        include('rodape.php');
+    ?>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
