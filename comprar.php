efb8e65d1ec01207cdcc8e5970de0fceb2667cc4
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-09-11 07:48:35 -0300

    att

--- comprar.php
+++ comprar.php
@@ -16,8 +16,7 @@ $quantidade_pedidos = $retorno_consulta->num_rows;
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/dieimes.css">
   <link rel="stylesheet" href="css/menu_dieimes.css">
@@ -32,39 +31,40 @@ $quantidade_pedidos = $retorno_consulta->num_rows;
   include('menu.php');
   ?>
   <div id="coontainer" class="container">
-  <?php
-  while ($bebidas = $retorno_consulta->fetch_assoc()) {
+    <?php
+    while ($bebidas = $retorno_consulta->fetch_assoc()) {
 
     ?>
-    <br>
-    <br>
-    <br>
-  <main class="product">
+      <br>
+      <br>
+      <br>
+      <main class="product">
         <div class="product-image">
-            <img src="<?php echo $bebidas['arquivo_caminho'];?>" alt="Produto Exemplo">
+          <img src="<?php echo $bebidas['arquivo_caminho']; ?>" alt="Produto Exemplo">
         </div>
         <div class="product-info">
-            <h2><?php echo $bebidas['nome_bebida']; ?></h2>
-            <p class="product-description"><?php echo $bebidas['descricao']; ?></p>
-            <p class="product-price"><?php echo $bebidas['valor'];?></p>
-            <div class="product-actions">
-                <button class="custom-btn" class="add-to-cart">Adicionar ao Carrinho</button>
-                <button class="custom-btn" class="buy-now">Comprar Agora</button>
-                <span class="favorite">&#9733;</span> <!-- Ícone de estrela para favoritar -->
-            </div>
+          <h2><?php echo $bebidas['nome_bebida']; ?></h2>
+          <p class="product-description"><?php echo $bebidas['descricao']; ?></p>
+          <p class="product-price"><?php echo $bebidas['valor']; ?></p>
+          <p class="product-price">Estoque Disponível: <?php echo $bebidas['quantidade']; ?></p>
+          <div class="product-actions">
+            <button class="custom-btn" class="add-to-cart">Adicionar ao Carrinho</button>
+            <button class="custom-btn" class="buy-now">Comprar Agora</button>
+            <span class="favorite">&#9733;</span> <!-- Ícone de estrela para favoritar -->
+          </div>
         </div>
-        <?php
-        }
-        ?>
-    </main>
+      <?php
+    }
+      ?>
+      </main>
   </div>
-  <br>
-  <div class="etrpaco"></div>
   <?php
-    include('rodape.php');
+  include('espacamento.php');
+  ?>
+  <?php
+  include('rodape.php');
   ?>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 
 </html>
