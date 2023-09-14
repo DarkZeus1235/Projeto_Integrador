9e708724e21d16a168de142edae8e8d697d142b9
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-09-14 09:01:25 -0300

    att

--- conta.php
+++ conta.php
@@ -11,6 +11,7 @@ $quantidade_cadastros = $retorno_consulta->num_rows;
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,11 +23,12 @@ $quantidade_cadastros = $retorno_consulta->num_rows;
     <link rel="icon" href="Imagens/icon.png">
     <title>Minha Conta</title>
 </head>
+
 <body>
     <?php
-        include('menu.php');
+    include('menu.php');
     ?>
-        <h1 class="h1_text">Minha Conta</h1>
+    <h1 class="h1_text">Minha Conta</h1>
 
     <div class="contaaiiner">
         <!-- Profile Section -->
@@ -35,19 +37,20 @@ $quantidade_cadastros = $retorno_consulta->num_rows;
             <div class="profile-info">
                 <div class="info-item">
                     <?php
-                        if(isset($_SESSION)){
+                    if (isset($_SESSION)) {
                     ?>
-                    <h3>Nome: <?php echo $_SESSION['nome'];?>
-                    </h3>
-                    <p>Email: <?php echo $_SESSION['email'];?></p>
+                        <h3>Nome: <?php echo $_SESSION['nome']; ?>
+                        </h3>
+                        <p>Email Cadastrado: <?php echo $_SESSION['email']; ?></p>
+                        <p>Senha Cadastrada: <?php echo $_SESSION['senha'];?></p>
                 </div>
                 <div class="info-item">
                     <h3>Endereço:</h3>
-                    <p><?php echo $_SESSION['endereco'];?></p>
+                    <p><?php echo $_SESSION['endereco']; ?></p>
                 </div>
-                <?php
-                }
-                ?>
+            <?php
+                    }
+            ?>
             </div>
         </div>
 
@@ -76,8 +79,9 @@ $quantidade_cadastros = $retorno_consulta->num_rows;
         </div>
     </div>
     <?php
-        include('rodape.php');
+    include('rodape.php');
     ?>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
 </html>
