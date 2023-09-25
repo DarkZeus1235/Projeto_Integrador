d211b63fad9db73c8efa769312ec8a83b78deda3
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-09-25 08:45:00 -0300

    att

--- vinhos.php
+++ vinhos.php
@@ -14,17 +14,6 @@ $quantidade_pedidos = $retorno_consulta->num_rows;
 <html lang="pt-br">
 
 <head>
-<<<<<<< HEAD
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"  integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="icon" href="Imagens/vikingpinguço.png">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/menu_dieimes.css">
-    <link rel="stylesheet" href="css/dieimes.css">
-    <link rel="icon" href="Imagens/Icon.png">
-=======
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -33,7 +22,6 @@ $quantidade_pedidos = $retorno_consulta->num_rows;
   <link rel="stylesheet" href="css/menu_dieimes.css">
   <link rel="stylesheet" href="css/dieimes.css">
   <link rel="icon" href="Imagens/vikingpinguço.png">
->>>>>>> main
   <title>Vinhos - Taverna de Valhalla</title>
 </head>
 
--- whisky.php
+++ whisky.php
@@ -17,20 +17,11 @@ $quantidade_pedidos = $retorno_consulta->num_rows;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/menu_dieimes.css">
-    <link rel="stylesheet" href="css/dieimes.css">
-    <link rel="icon" href="Imagens/Icon.png">
-=======
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <link rel="stylesheet" href="css/menu_dieimes.css">
   <link rel="stylesheet" href="css/dieimes.css">
     <link rel="icon" href="Imagens/vikingpinguço.png">
->>>>>>> main
     <title>Whisky - Taverna de Valhalla</title>
 </head>
 
