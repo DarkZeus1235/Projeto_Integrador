b75186fd0f32daeefd11b70f49d5f7a36904f764
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-10-04 08:34:18 -0300

    att

--- dashboard.php
+++ dashboard.php
@@ -1,19 +1,20 @@
 <?php
-    include('conexao.php');
+include('conexao.php');
 
-    require('protect/protect_adm.php');
+require('protect/protect_adm.php');
 
-    if(!isset($_SESSION)){
-        session_start();
-    }
+if (!isset($_SESSION)) {
+    session_start();
+}
 
-    $consultar_banco = "SELECT * FROM cadastro";
+$consultar_banco = "SELECT * FROM cadastro";
 
-    $retorno_consulta = $mysqli->query($consultar_banco) or die($mysqli->error);
-    $quantidade_cadastros = $retorno_consulta->num_rows;
+$retorno_consulta = $mysqli->query($consultar_banco) or die($mysqli->error);
+$quantidade_cadastros = $retorno_consulta->num_rows;
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -25,50 +26,61 @@
     <link rel="stylesheet" href="css/menu_dieimes.css">
     <link rel="icon" href="Imagens/icon.png">
 </head>
+
 <body>
     <?php
-        include('menu_dashboard.php');
+    include('menu_dashboard.php');
     ?>
     <br>
     <br>
-    <div class="container">
     <main>
-        <h2>Bem-vindo ao Dashboard de Administradores!</h2>,
-        </div>
-        <h2 id="alinhar_dashboard">Contas cadastradas no Sistema: </h2>
-        <table id="table" class="table table-striped">
-            <tr>
-                <th>ID da Conta:</th>
-                <th>Nome:</th>
-                <th>Email:</th>
-                <th>Senha:</th>
-                <th>Usuário:</th>
-                <th>Funcionalidades:</th>
-                <th>Funcionalidades:</th>
-
-            </tr>
-            <?php
-                        while($logins = $retorno_consulta -> fetch_assoc()){
-                       ?>
-                        <tr>
-                        <td><?php echo $logins['id_login'];?></td>
-                        <td><?php echo $logins['nome'];?> </td>
-                        <td><?php echo $logins['email'];?> </td>
-                        <td><?php echo $logins['senha'];?> </td>
-                        <td><?php echo $logins['username'];?></td>  
-                        <td><a id="butao-adm" class="btn btn-danger" href="deletar.php?codigo_cadastro=<?php echo $logins['id_login'];?>">Deletar</a></td>
-                        <td><a id="butao-adm" class="btn btn-primary" href="alterar.php">Alterar</a>
+        <h2>Bem-vindo ao Dashboard de Administradores!</h2>
+    </main>
+    <div class="contaaiiner">
+        <div id="alinhar_dashboard" class="table-responsive"> <!-- Adicione a classe 'table-responsive' para tornar a tabela responsiva -->
+            <h2>Contas cadastradas no Sistema:</h2>
+            <div class="table-responsive">
+            <table id="table" class="table table-striped">
+                <thead> <!-- Adicione um cabeçalho de tabela -->
+                    <tr>
+                        <th>ID da Conta:</th>
+                        <th>Nome:</th>
+                        <th>Email:</th>
+                        <th>Senha:</th>
+                        <th>Usuário:</th>
+                        <th>Funcionalidades:</th>
+                        <th>Funcionalidades:</th>
                     </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($logins = $retorno_consulta->fetch_assoc()) {
+                    ?>
+                        <tr>
+                            <td><?php echo $logins['id_login']; ?></td>
+                            <td><?php echo $logins['nome']; ?> </td>
+                            <td><?php echo $logins['email']; ?> </td>
+                            <td><?php echo $logins['senha']; ?> </td>
+                            <td><?php echo $logins['username']; ?></td>
+                            <td class="text-center">
+                                <a id="butao-adm" class="btn btn-danger" href="deletar.php?codigo_cadastro=<?php echo $logins['id_login']; ?>">Deletar</a>
+                            </td>
+                            <td class="text-center">
+                                <a id="butao-adm" class="btn btn-primary" href="alterar.php">Alterar</a>
+                            </td>
+                        </tr>
                     <?php
                     }
-
-
                     ?>
-        </table>
-    </main>
+                </tbody>
+            </table>
+            </div>
+        </div>
+    </div>
     <?php
-        include('rodape.php');
+    include('rodape.php');
     ?>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </html>
