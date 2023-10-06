de069c9fb7da3fe3a2d99ddb14f273acbc0ad30c
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-10-06 07:11:31 -0300

    att

--- conta.php
+++ conta.php
@@ -10,6 +10,13 @@ if (!isset($_SESSION)) {
 if (isset($_SESSION['id_login'])) {
     $id = $_SESSION['id_login'];
 
+
+$consultar_banco = "SELECT * FROM cadastro";
+
+$retorno_consulta = $mysqli->query($consultar_banco) or die($mysqli->error);
+$quantidade_cadastros = $retorno_consulta->num_rows;
+
+
     /* Consultar o banco de dados para obter informações do usuário */
     $stmt = $mysqli->prepare("SELECT * FROM cadastro WHERE id_login = ? LIMIT 1");
     $stmt->bind_param("s", $id);
@@ -25,8 +32,58 @@ if (isset($_SESSION['id_login'])) {
         $_SESSION['endereco'] = $usuario['endereco'];
         $_SESSION['email'] = $usuario['email'];
         $_SESSION['senha'] = $usuario['senha'];
+        // Verificar se 'endereco' está definido no resultado antes de atribuir à variável de sessão
+        if (isset($usuario['endereco'])) {
+            $_SESSION['endereco'] = $usuario['endereco'];
+        } else {
+            $_SESSION['endereco'] = ""; // Defina como uma string vazia ou outro valor padrão
+        }
     }
-} 
+    }
+
+if (isset($_FILES["foto"])) {
+
+    // Verifique se o arquivo é uma imagem
+    $check = getimagesize($_FILES["foto"]["tmp_name"]);
+    if ($check === false) {
+        die("O arquivo não é uma imagem.");
+    }
+
+    // Verifique a extensão do arquivo
+    $extensoesPermitidas = array('jpeg', 'jpg', 'png', 'gif');
+    $extensaoArquivo = strtolower(pathinfo($_FILES["foto"]["name"], PATHINFO_EXTENSION));
+    if (!in_array($extensaoArquivo, $extensoesPermitidas)) {
+        die("Tipo de arquivo não suportado.");
+    }
+
+    // Verifique o tamanho do arquivo (por exemplo, limite de 5MB aqui)
+    if ($_FILES["foto"]["size"] > 5000000) {
+        die("Arquivo muito grande!! Max: 5MB");
+    }
+
+    // Defina o local para salvar a imagem
+    $diretorioUpload = "Imagens/ftperfil/";
+    $novoNomeArquivo = uniqid() . "." . $extensaoArquivo;
+    $caminhoFinal = $diretorioUpload . $novoNomeArquivo;
+
+    // Tente mover o arquivo temporário para o diretório final
+    if (!move_uploaded_file($_FILES["foto"]["tmp_name"], $caminhoFinal)) {
+        die("Ocorreu um erro ao fazer o upload da imagem.");
+    }
+
+    // Atualize o caminho da imagem no banco de dados
+    $stmt = $mysqli->prepare("UPDATE cadastro SET foto_perfil_caminho = ? WHERE id_login = ?");
+    $stmt->bind_param("ss", $caminhoFinal, $id);
+    if (!$stmt->execute()) {
+        die("Erro ao atualizar o caminho da imagem no banco de dados.");
+    }
+
+
+
+    // Atualize a variável de sessão para refletir a mudança feita
+    $_SESSION["foto_perfil_caminho"] = $caminhoFinal;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -103,10 +160,10 @@ if (isset($_SESSION['id_login'])) {
         <main>
             <h2 class="mb-3">Minhas informações:</h2>
             <p><span class="info-title">Nome:</span> <?php echo $_SESSION["nome"]; ?></p>
-            <p><span class="info-title">Endereço:</span> <?php echo $_SESSION['endereco']; ?></p>
+            <p><span class="info-title">Endereço:</span> <?php echo $usuario['endereco']; ?></p>
             <p><span class="info-title">Email:</span> <?php echo $_SESSION["email"]; ?></p>
             <div class="text-center mt-5">
-                <p><a href="sair.php" class="btn btn-danger">Sair</a></p>
+                <p><a href="logout.php" class="btn btn-danger">Sair</a></p>
         </main>
 
         <!-- Orders Section -->
