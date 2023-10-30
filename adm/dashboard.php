aa38dcc1736e5d9e41dd22ca850e458d1fa357d7
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-10-30 09:05:20 -0300

    att

--- adm/dashboard.php
+++ adm/dashboard.php
@@ -1,7 +1,7 @@
 <?php
-include('conexao.php');
+include('../static/conexao.php');
 
-require('protect/protect_adm.php');
+require('../protect/protect_adm.php');
 
 if (!isset($_SESSION)) {
     session_start();
@@ -21,15 +21,15 @@ $quantidade_cadastros = $retorno_consulta->num_rows;
     <link rel="stylesheet" href="style.css">
     <title>Dashboard de Administradores</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/dieimes.css">
-    <link rel="stylesheet" href="css/menu_dieimes.css">
-    <link rel="icon" href="Imagens/icon.png">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/dieimes.css">
+    <link rel="stylesheet" href="../css/menu_dieimes.css">
+    <link rel="icon" href="../Imagens/icon.png">
 </head>
 
 <body>
     <?php
-    include('menu.php');
+    include('../static/menu.php');
     ?>
     <br>
     <br>
@@ -41,7 +41,7 @@ $quantidade_cadastros = $retorno_consulta->num_rows;
         <div class="row-fluid">
         <div class="card-group">
   <div class="card">
-    <img href="contas_sistema.php" src="Imagens/Viking_conta_resized.png" class="card-img-top" alt="...">
+    <img href="contas_sistema.php" src="../Imagens/Viking_conta_resized.png" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Contas Cadastradas no Sistema.</h5>
       <p class="card-text">Verificar todas as contas cadastradas no sistema.</p>
@@ -50,7 +50,7 @@ $quantidade_cadastros = $retorno_consulta->num_rows;
     
   </div>
   <div class="card">
-    <img href="mensagens_sistema.php" src="Imagens/Viking_contato.jpg" class="card-img-top" alt="...">
+    <img href="mensagens_sistema.php" src="../Imagens/Viking_contato.jpg" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Mensagens de Contato</h5>
       <p class="card-text">Verificar todas as mensagens de contato enviadas pelo formulário.</p>
@@ -59,7 +59,7 @@ $quantidade_cadastros = $retorno_consulta->num_rows;
     
   </div>
   <div class="card">
-    <img href="pedidos_sistema.php" src="Imagens/Viking_compra.jpg" class="card-img-top" alt="...">
+    <img href="pedidos_sistema.php" src="../Imagens/Viking_compra.jpg" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Pedidos</h5>
       <p class="card-text">Ver todos os pedidos realizados.</p>
@@ -71,10 +71,10 @@ $quantidade_cadastros = $retorno_consulta->num_rows;
         </div>
     </div>
     <?php 
-        include('espacamento.php');
+        include('../static/espacamento.php');
     ?>
     <?php
-    include('rodape.php');
+    include('../static/rodape.php');
     ?>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
